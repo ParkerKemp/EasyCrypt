f87983eb451e356ad8198de3c8bf9c15c4b2848d
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-10-03 03:09:13 -0500

    Use RSA, mavenize

--- src/com/spinalcraft/easycrypt/EasyCrypt.java
+++ src/com/spinalcraft/easycrypt/EasyCrypt.java
@@ -1,6 +1,7 @@
 package com.spinalcraft.easycrypt;
 
 import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -12,9 +13,14 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public abstract class EasyCrypt {
 	public KeyPair generateKeys() throws NoSuchAlgorithmException {
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("DSA");
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
         generator.initialize(1024, random);
         return generator.generateKeyPair();
@@ -23,7 +29,7 @@ public abstract class EasyCrypt {
     public PrivateKey loadPrivateKey(String key64) throws GeneralSecurityException {
         byte[] clear = decode(key64);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
-        KeyFactory fact = KeyFactory.getInstance("DSA");
+        KeyFactory fact = KeyFactory.getInstance("RSA");
         PrivateKey priv = fact.generatePrivate(keySpec);
         Arrays.fill(clear, (byte) 0);
         return priv;
@@ -32,14 +38,13 @@ public abstract class EasyCrypt {
     public PublicKey loadPublicKey(String stored) throws GeneralSecurityException {
         byte[] data = decode(stored);
         X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
-        KeyFactory fact = KeyFactory.getInstance("DSA");
+        KeyFactory fact = KeyFactory.getInstance("RSA");
         return fact.generatePublic(spec);
     }
 
     public String stringFromPrivateKey(PrivateKey priv) throws GeneralSecurityException {
-        KeyFactory fact = KeyFactory.getInstance("DSA");
-        PKCS8EncodedKeySpec spec = fact.getKeySpec(priv,
-                PKCS8EncodedKeySpec.class);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec spec = fact.getKeySpec(priv, PKCS8EncodedKeySpec.class);
         byte[] packed = spec.getEncoded();
         String key64 = encode(packed);
 
@@ -48,11 +53,21 @@ public abstract class EasyCrypt {
     }
 
     public String stringFromPublicKey(PublicKey publ) throws GeneralSecurityException {
-
-        KeyFactory fact = KeyFactory.getInstance("DSA");
-        X509EncodedKeySpec spec = fact.getKeySpec(publ,
-                X509EncodedKeySpec.class);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec spec = fact.getKeySpec(publ, X509EncodedKeySpec.class);
         return encode(spec.getEncoded());
+    }
+    
+    public byte[] encrypt(PublicKey key, byte[] plaintext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(plaintext);
+    }
+
+    public byte[] decrypt(PrivateKey key, byte[] ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(ciphertext);
     }
     
     protected abstract byte[] decode(String str);
